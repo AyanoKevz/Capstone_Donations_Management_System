8e532711db82cb5ae2202ec9c41d58cf1a2d7142
AyanoKevz <kevin25.cloudspace@gmail.com>
2025-02-17 04:11:23 +0800

    DELETE

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -17,7 +17,6 @@
   <link rel="stylesheet" href="{{ asset('lib/bootstrap/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('lib/datatables/datatables.min.css') }}">
   <link rel="stylesheet" href="{{ asset ('assets/admin/css/list.css') }}">
-  <link rel="stylesheet" href="{{ asset ('assets/admin/css/publish_news.css') }}">
 
 </head>
 
